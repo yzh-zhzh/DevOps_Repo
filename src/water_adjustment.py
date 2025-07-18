2eef2739a5e9843a708fd090ea27665f02b356b1
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-07-18 10:35:55 +0800

    Updated water adjustment file for LCD function

--- src/water_adjustment.py
+++ src/water_adjustment.py
@@ -2,6 +2,8 @@ import time
 from hal import hal_adc as adc
 from hal import hal_servo as servo
 from hal import hal_lcd as LCD
+from lcd_display_controller import update_water_volume
+
 
 def water_adjustment_thread(system_state):
     lcd = LCD.lcd()
@@ -10,5 +12,7 @@ def water_adjustment_thread(system_state):
         pot_val = int((pot_val / 1023) * 100)
         if system_state['fire_detected'] and not system_state['system_override']:
             servo.set_servo_position(pot_val)
-            lcd.lcd_display_string(f"Water Level: {pot_val}%", 2)
+            update_water_volume(f"Water Level: {pot_val}%")
         time.sleep(1)
+
+        
