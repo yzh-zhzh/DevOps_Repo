ed4f49488e37c6bf9f545262453ebc269cad5de8
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-07-18 20:32:02 +0800

    Modified keypad file code to fix passcode issue of system

--- src/keypad_manual_override.py
+++ src/keypad_manual_override.py
@@ -13,8 +13,8 @@ import queue
 
 def keypad_manual_override_thread(system_state):
     shared_keypad_queue = system_state['shared_keypad_queue']
+    import lcd_display_controller
     password = ''
-    awaiting_password = False
 
     while True:
         try:
@@ -22,16 +22,13 @@ def keypad_manual_override_thread(system_state):
 
             if key == '*':
                 if system_state['fire_detected']:
-                    awaiting_password = True
-                    set_awaiting_password(True)
                     password = ''
-                    with lcd_lock:
-                        update_lcd_line2("Enter Password:")
+                    set_awaiting_password(True)
+                    # The display thread will now show FIRE DETECTED! and Enter Passcode:
 
-            elif str(key).isdigit() and awaiting_password:
+            elif str(key).isdigit() and lcd_display_controller.awaiting_password:
                 password += key
-                with lcd_lock:
-                    update_lcd_line2(f"Entered: {password}")
+                # The display thread will show Entered: ... if you want, but for now, just update state
 
                 if len(password) >= 4:
                     if password == "1234":
@@ -41,16 +38,19 @@ def keypad_manual_override_thread(system_state):
                         dc_motor.stop()
                         servo.set_servo_position(0)
                         set_override_mode(True)
-                        with lcd_lock:
-                            update_lcd_line1("Override Success")
-                            update_lcd_line2("")
+                        set_awaiting_password(False)
+                        # Show success message for 5 seconds directly
+                        update_lcd_line1("Success!")
+                        update_lcd_line2("Deactivated")
+                        time.sleep(5)
+                        # After 5 seconds, the display thread will show the default screen
+                        password = ''
                     else:
-                        with lcd_lock:
-                            update_lcd_line1("Wrong Passcode")
-
-                    password = ''
-                    awaiting_password = False
-                    set_awaiting_password(False)
+                        # Show wrong passcode message for 5 seconds, then re-prompt
+                        update_lcd_line2("Wrong Passcode!")
+                        time.sleep(5)
+                        password = ''
+                        set_awaiting_password(True)
 
         except queue.Empty:
             pass
