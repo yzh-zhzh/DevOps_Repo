a45187f3de8346d6df4a77f9483169c52e09b6bd
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-07-18 12:15:13 +0800

    Created LCD function to display messages on LCD

---
+++ src/lcd_display_controller.py
@@ -0,0 +1,56 @@
+import time
+from hal import hal_lcd as LCD
+
+# Shared variables to be updated externally
+sprinkler_status_message = "Water OK"
+water_volume_message = "Vol: 0%"
+entered_override_mode = False
+awaiting_password = False
+fire_detected = False
+
+def update_sprinkler_status(message):
+    global sprinkler_status_message
+    sprinkler_status_message = message[:16]
+
+def update_water_volume(vol_str):
+    global water_volume_message
+    water_volume_message = vol_str[:16]
+
+def set_override_mode(status):
+    global entered_override_mode
+    entered_override_mode = status
+
+def set_awaiting_password(status):
+    global awaiting_password
+    awaiting_password = status
+
+def set_fire_detected(status):
+    global fire_detected
+    fire_detected = status
+
+def lcd_display_thread():
+    lcd = LCD.lcd()
+    alternate = 0
+
+    while True:
+        lcd.lcd_clear()
+        if fire_detected and not entered_override_mode:
+            lcd.lcd_display_string("FIRE DETECTED!", 1)
+
+            if awaiting_password:
+                lcd.lcd_display_string("Enter Password:", 2)
+            else:
+                if alternate == 0:
+                    lcd.lcd_display_string(water_volume_message, 2)
+                elif alternate == 1:
+                    lcd.lcd_display_string(sprinkler_status_message, 2)
+                else:
+                    lcd.lcd_display_string("* to override!", 2)
+
+                alternate = (alternate + 1) % 3
+            time.sleep(5)
+        
+        else:
+            lcd.lcd_display_string("Smart Fire Alert", 1)
+            lcd.lcd_display_string("System Ready!", 2)
+            time.sleep(1)
