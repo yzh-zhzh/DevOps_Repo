4cc50dcd5912551356d94bf49c93c3b8a1179dc4
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-07-22 09:56:50 +0800

    Edited keypad code to fix issue: passcode entered but no action thereafter

--- src/keypad_manual_override.py
+++ src/keypad_manual_override.py
@@ -7,7 +7,6 @@ from lcd_display_controller import (
     set_awaiting_password,
     update_lcd_line1,
     update_lcd_line2,
-    lcd_lock
 )
 import queue
 
@@ -24,33 +23,34 @@ def keypad_manual_override_thread(system_state):
                 if system_state['fire_detected']:
                     password = ''
                     set_awaiting_password(True)
-                    # The display thread will now show FIRE DETECTED! and Enter Passcode:
+                    update_lcd_line1("Enter Passcode:")
+                    update_lcd_line2("")
 
             elif str(key).isdigit() and lcd_display_controller.awaiting_password:
-                password += key
-                # The display thread will show Entered: ... if you want, but for now, just update state
+                if len(password) < 8:  # Limit passcode length for display
+                    password += key
+                update_lcd_line2(password)
 
-                if len(password) >= 4:
-                    if password == "1234":
-                        system_state['fire_detected'] = False
-                        system_state['system_override'] = True
-                        buzzer.turn_off()
-                        dc_motor.stop()
-                        servo.set_servo_position(0)
-                        set_override_mode(True)
-                        set_awaiting_password(False)
-                        # Show success message for 5 seconds directly
-                        update_lcd_line1("Success!")
-                        update_lcd_line2("Deactivated")
-                        time.sleep(5)
-                        # After 5 seconds, the display thread will show the default screen
-                        password = ''
-                    else:
-                        # Show wrong passcode message for 5 seconds, then re-prompt
-                        update_lcd_line2("Wrong Passcode!")
-                        time.sleep(5)
-                        password = ''
-                        set_awaiting_password(True)
+            elif key == '#' and lcd_display_controller.awaiting_password:
+                if password == "1234":
+                    system_state['fire_detected'] = False
+                    system_state['system_override'] = True
+                    buzzer.turn_off()
+                    dc_motor.stop()
+                    servo.set_servo_position(0)
+                    set_override_mode(True)
+                    set_awaiting_password(False)
+                    # Show system ready message
+                    update_lcd_line1("Smart Fire System")
+                    update_lcd_line2("System Ready!")
+                    password = ''
+                else:
+                    # Show wrong passcode message for 2 seconds, then re-prompt
+                    update_lcd_line1("Wrong Passcode!")
+                    update_lcd_line2("Pls Try Again!")
+                    time.sleep(2)
+                    update_lcd_line1("Enter Passcode:")
+                    update_lcd_line2(password)
 
         except queue.Empty:
             pass
