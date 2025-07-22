63e43087b50a2f7ba87fef39bb173c6606df753f
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-07-22 19:51:26 +0800

    Modified code to fix error in code - water adjustment dc motor

--- src/water_adjustment.py
+++ src/water_adjustment.py
@@ -1,9 +1,11 @@
 import time
 from hal import hal_adc as adc
 from hal import hal_servo as servo
+from hal import hal_dc_motor as dc_motor
 from lcd_display_controller import update_water_volume
 
 def water_adjustment_thread(system_state):
+    dc_motor.init()
     while True:
         pot_val = adc.get_adc_value(1)
         pot_val = int((pot_val / 1023) * 100)
