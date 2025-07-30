d4584db03172f32dc94b08cca6ff8c737631e57c
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-07-30 10:10:19 +0800

    Refactor water adjustment logic to control DC motor speed and servo position based on system state

--- src/water_adjustment.py
+++ src/water_adjustment.py
@@ -1,15 +1,30 @@
+
 import time
 from hal import hal_adc as adc
 from hal import hal_servo as servo
+from hal import hal_dc_motor as dc_motor
 from lcd_display_controller import update_water_volume
 
 def water_adjustment_thread(system_state):
+    sweep_pos = 0
+    sweep_dir = 1
     while True:
         pot_val = adc.get_adc_value(1)
         pot_val = int((pot_val / 1023) * 100)
+        # Potentiometer always controls DC motor speed
+        dc_motor.set_motor_speed(pot_val)
+        update_water_volume(f"Water Lvl: {pot_val}%")
+
+        # If fire detected and not overridden, servo sweeps back and forth
         if system_state['fire_detected'] and not system_state['system_override']:
-            servo.set_servo_position(pot_val)
-            update_water_volume(f"Water Lvl: {pot_val}%")
-        time.sleep(1)
+            servo.set_servo_position(sweep_pos)
+            sweep_pos += 10 * sweep_dir
+            if sweep_pos >= 100:
+                sweep_pos = 100
+                sweep_dir = -1
+            elif sweep_pos <= 0:
+                sweep_pos = 0
+                sweep_dir = 1
+        time.sleep(0.1)
 
         
