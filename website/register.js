fbb793f8c8d03392743de7d947e650c11bc66239
meena <meenakshi.24@ichat.sp.edu.sg>
2025-07-30 14:22:53 +0800

    authentication file using firebase

---
+++ website/register.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBbPSfgU4hYMyPEM9S9L8z5xFx7XdMVWeo",
+    authDomain: "fire-alarm-system-77832.firebaseapp.com",
+    projectId: "fire-alarm-system-77832",
+    storageBucket: "fire-alarm-system-77832.firebasestorage.app",
+    messagingSenderId: "233304506178",
+    appId: "1:233304506178:web:c479e2b9bf475ad17e12db"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
