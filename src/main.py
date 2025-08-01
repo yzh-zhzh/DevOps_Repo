f83c02b822fa3ac2e04bd7889991ca74048a8ecf
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-08-01 14:46:43 +0800

    Implement motor lock mechanism and update LCD display for passcode prompts

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import queue
 from hal import hal_led as led
 from hal import hal_lcd as LCD
 from hal import hal_adc as adc
-from hal import hal_buzzer as buzzer
+# from hal import hal_buzzer as buzzer
 from hal import hal_keypad as keypad
 from hal import hal_moisture_sensor as moisture_sensor
 from hal import hal_servo as servo
@@ -23,7 +23,7 @@ from notify_alert import notify_fire_alert
 from keypad_manual_override import keypad_manual_override_thread
 from water_adjustment import water_adjustment_thread
 from sprinkler_confirmation import moisture_sensor_sprinkler_confirmation_thread
-from play_fire_tone import play_fire_alert_tone
+# from play_fire_tone import play_fire_alert_tone
 
 # Shared resources
 shared_keypad_queue = queue.Queue()
@@ -43,7 +43,7 @@ def main():
     # Initialize all hardware components
     led.init()
     adc.init()
-    buzzer.init()
+    # buzzer.init()
     moisture_sensor.init()
     dc_motor.init()
     servo.init()
@@ -64,7 +64,7 @@ def main():
     Thread(target=keypad_manual_override_thread, args=(system_state,), daemon=True).start()
     Thread(target=water_adjustment_thread, args=(system_state,), daemon=True).start()
     Thread(target=moisture_sensor_sprinkler_confirmation_thread, args=(system_state,), daemon=True).start()
-    Thread(target=play_fire_alert_tone, args=(system_state,), daemon=True).start()
+    # Thread(target=play_fire_alert_tone, args=(system_state,), daemon=True).start()
     Thread(target=lcd_display_thread, daemon=True).start()
     Thread(target=notify_fire_alert, args=(system_state,), daemon=True).start()
 
