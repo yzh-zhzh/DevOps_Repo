a658a02e32ee483620edce08dcc73f57f578f54d
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-08-17 21:26:41 +0800

    Update main.py to comment out unused imports and threads

--- src/main.py
+++ src/main.py
@@ -12,8 +12,10 @@ from hal import hal_moisture_sensor as moisture_sensor
 from hal import hal_servo as servo
 from hal import hal_temp_humidity_sensor as temp_humid_sensor
 from hal import hal_dc_motor as dc_motor
-from hal import hal_rfid_reader
+#from hal import hal_rfid_reader
 from lcd_display_controller import lcd_display_thread
+from slide_switch import wait_for_switch_on
+from hal import hal_input_switch
 
 from temp_sensor import fire_detection_thread
 from notify_alert import notify_fire_alert
@@ -22,7 +24,7 @@ from water_adjustment import water_adjustment_thread
 from sprinkler_confirmation import moisture_sensor_sprinkler_confirmation_thread
 from play_fire_tone import play_fire_alert_tone
 from ultrasonic_data import ultrasonic_data_thread
-from Camera import run_camera_server
+#from Camera import run_camera_server
 
 shared_keypad_queue = queue.Queue()
 
@@ -62,22 +64,19 @@ def start_all_threads():
     Thread(target=lcd_display_thread, daemon=True).start()
     Thread(target=notify_fire_alert, args=(system_state,), daemon=True).start()
     Thread(target=ultrasonic_data_thread, args=(system_state,), daemon=True).start()
-    Thread(target=run_camera_server, daemon=True).start()
+    #Thread(target=run_camera_server, daemon=True).start()
 
 def main():
-    print("Waiting for RFID card to activate system...")
-    while True:
-        if RFID.RFID_reader():
-            print("RFID detected! Activating all systems.")
-            break
-        time.sleep(1)
+
+    # Initialize slide switch hardware
+    hal_input_switch.init()
+    wait_for_switch_on()
 
     initialize_hardware()
     start_all_threads()
 
     print("All sensors and systems are now running in the background.")
 
-    
     while True:
         time.sleep(1)
 
